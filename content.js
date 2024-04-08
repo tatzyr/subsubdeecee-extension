6e4d6ca1a036bd4e4d8fb2ff6b5063d85b9c9f78
Tatsuya Otsuka <tatzyr@gmail.com>
2024-04-09 01:04:25 +0900

    Parallelize fetch of subtitle files

--- content.js
+++ content.js
@@ -32,22 +32,29 @@
       return;
     }
 
-    for (const lang of languages) {
-      /** @type {FetchSubtitlesRequest} */
-      const message = { action: "fetchSubtitles", payload: { event, lang, videoId } };
-      /** @type {FetchSubtitlesResponse} */
-      const { data: vttText, error } = await b.runtime.sendMessage(message);
-      if (error) {
-        console.info("[SubSubDeeCee] Failed to fetch subtitles.", error);
-        continue;
-      }
-      const track = document.createElement("track");
-      track.setAttribute("src", URL.createObjectURL(new Blob([vttText], { type: "text/vtt" })));
-      track.setAttribute("srclang", lang);
-      const langLabel = new Intl.DisplayNames([], { type: "language" }).of(lang);
-      track.setAttribute("label", `${langLabel} (SubSubDeeCee)`);
+    const tracks = await Promise.all(
+      languages.map(async (lang) => {
+        /** @type {FetchSubtitlesRequest} */
+        const message = { action: "fetchSubtitles", payload: { event, lang, videoId } };
+        /** @type {FetchSubtitlesResponse} */
+        const { data: vttText, error } = await b.runtime.sendMessage(message);
+        if (error) {
+          console.info("[SubSubDeeCee] Failed to fetch subtitles.", error);
+          return null;
+        }
+        const track = document.createElement("track");
+        track.setAttribute("src", URL.createObjectURL(new Blob([vttText], { type: "text/vtt" })));
+        track.setAttribute("srclang", lang);
+        const langLabel = new Intl.DisplayNames([], { type: "language" }).of(lang);
+        track.setAttribute("label", `${langLabel} (SubSubDeeCee)`);
+        return track;
+      }),
+    );
 
-      video.appendChild(track);
+    for (const track of tracks) {
+      if (track) {
+        video.appendChild(track);
+      }
     }
   }
 
